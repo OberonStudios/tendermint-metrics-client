c0689efc5fec29c6f772d4d9004df7979debc98c
aakatev <aakatev@mail.ccsf.edu>
2018-12-15 00:05:44 -0800

    Flexbox added

--- app.js
+++ app.js
@@ -4,37 +4,39 @@ var cors = require('cors')
 var app = express()
 var port = 3000
 var path    = require("path");
-var data_name = 'tendermint_data.json'
+var data_url = 'https://raw.githubusercontent.com/OberonStudios/tendermint-metrics-client/master/tendermint_data.json'
 
+// Routes
+// Dasboard & redirect to dashboard
 app.get('/dashboard', function (req, res) {
     res.sendFile(path.join(__dirname+'/dashboard.html'));
 });
-    
 
-//  Send json, use cors 
-app.get('/send_json', cors(), function (req, res) {
-    res.sendFile(path.join(__dirname+'/'+data_name));
-});
-
-//  Send bulma css
-app.get('/css/bulma', function (req, res) {
-    res.sendFile(path.join(__dirname+'/node_modules/bulma/css/bulma.min.css'));
-});
-
-
-// Redirect
 app.get('/', async function (req, res) {
     res.redirect('/dashboard');
 });
 
+//  Send json, use cors 
+app.get('/send_json', cors(), function (req, res) {
+    res.sendFile(data_url);
+});
+
+//  Send bulma css && custom css
+app.get('/css/bulma', function (req, res) {
+    res.sendFile(path.join(__dirname+'/node_modules/bulma/css/bulma.min.css'));
+});
+
+app.get('/css/custom', function (req, res) {
+    res.sendFile(path.join(__dirname+'/css/custom.css'));
+});
+
 // Remove tendermint json cache
-app.get('/clean-cache', function(req, res) {
+app.get('/clean_cache', function(req, res) {
     fs.unlink(data_name, (err) => {
         if (err) throw err;
             console.log(`${data_name} was removed`);
     });
-    res.send(`<h2>${data_name} was removed</h2>`)
-})
+    res.redirect('/')
+});
 
-
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+app.listen(port, () => console.log(`Listening on port ${port}!`));
